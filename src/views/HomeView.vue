2db0b63400f0921119f757568ad3c4b2adf0f5b9
Sébastien COLLET <seb.collet@gmail.com>
2025-06-25 22:14:55 +0200

    Mise en place src base + router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<!-- Composant HomeView.vue -->
+
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Page d'accueil</h1>
   </main>
 </template>
